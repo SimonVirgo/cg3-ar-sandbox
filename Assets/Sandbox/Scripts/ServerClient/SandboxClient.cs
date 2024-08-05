9e1d33e7a8e29ca4d41328f47e7cd0746b8475b7
Simon <simon@terranigma-solutions.com>
2024-08-05 17:39:09 +0200

    [POC] write texture to rendertexture in shader

--- Assets/Sandbox/Scripts/ServerClient/SandboxClient.cs
+++ Assets/Sandbox/Scripts/ServerClient/SandboxClient.cs
@@ -7,6 +7,9 @@ namespace Sandbox.Scripts.ServerClient
     public class SandboxClient: MonoBehaviour
     {
         public ARSandbox.Sandbox Sandbox;
+        public Shader ServerShader;
+        private RenderTexture _serverRenderTexture;
+        private SandboxDescriptor sandboxDescriptor;
         
         private void Setup()
         {
@@ -15,11 +18,39 @@ namespace Sandbox.Scripts.ServerClient
 
         private void OnEnable()
         {
+            Sandbox.SetSandboxShader(ServerShader);
+            Sandbox.SetShaderTexture("_FireSurfaceTex", _serverRenderTexture);
             Debug.Log("Server Sandbox Enabled");
         }
 
+        private void FillTextureWithRandomColor()
+        {
+            if (_serverRenderTexture == null)
+            {
+                _serverRenderTexture = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGB32);
+                _serverRenderTexture.Create();
+            }
+
+            Color randomColor = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value, 1.0f);
+            Texture2D tempTexture = new Texture2D(_serverRenderTexture.width, _serverRenderTexture.height, TextureFormat.ARGB32, false);
+            Color[] colors = new Color[tempTexture.width * tempTexture.height];
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colors[i] = randomColor;
+            }
+            tempTexture.SetPixels(colors);
+            tempTexture.Apply();
+
+            RenderTexture.active = _serverRenderTexture;
+            Graphics.Blit(tempTexture, _serverRenderTexture);
+            RenderTexture.active = null;
+
+            Destroy(tempTexture);
+        }
         private void Update()
         {
+            FillTextureWithRandomColor();
+            Sandbox.SetShaderTexture("_FireSurfaceTex", _serverRenderTexture);
             
         }
         
